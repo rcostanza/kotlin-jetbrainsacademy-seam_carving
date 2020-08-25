fc115220db7267a411c7938422c216a250221fea
Ronaldo Alves <setembro@gmail.com>
2020-08-24 21:01:00 -0300

    Stage 2

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -1,27 +1,21 @@
 package seamcarving
 
 import java.awt.Color
-import java.awt.image.BufferedImage
 import java.io.File
-import java.util.*
 import javax.imageio.ImageIO
 
-val s = Scanner(System.`in`)
+fun main(args: Array<String>) {
+    val inputImage = args[args.indexOf("-in")+1]
+    val outputImage = args[args.indexOf("-out")+1]
 
-fun main() {
-    println("Enter rectangle width:")
-    val w = s.nextLine().toInt()
-    println("Enter rectangle height:")
-    val h = s.nextLine().toInt()
-    println("Enter output image name:")
-    val imageName = s.nextLine()
+    val buffer = ImageIO.read(File(inputImage))
 
-    val imageBuffer = BufferedImage(w, h, BufferedImage.TYPE_INT_RGB)
-    val graphics = imageBuffer.createGraphics()
-
-    graphics.color = Color.RED
-    graphics.drawLine(0, 0, w-1, h-1)
-    graphics.drawLine(0, h-1, w-1, 0)
-
-    ImageIO.write(imageBuffer, "png", File(imageName))
+    for(x in 0 until buffer.width) {
+        for(y in 0 until buffer.height) {
+            val color = Color(buffer.getRGB(x, y))
+            buffer.setRGB(x, y, Color(255 - color.red, 255 - color.green, 255 - color.blue).rgb)
+        }
+    }
+    
+    ImageIO.write(buffer, "png", File(outputImage))
 }
