ec73ff7d8b0d0b01bd28d4482a0001a11b2c935d
Ronaldo Alves <setembro@gmail.com>
2020-08-28 06:21:03 -0300

    Stage 4

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -13,14 +13,9 @@ fun main(args: Array<String>) {
 
     val buffer = ImageIO.read(File(inputImage))
     val energyBuffer = buffer.energyBuffer()
-    val maxEnergy = energyBuffer.maxEnergy
 
-    (0 until buffer.width).forEach { col ->
-        (0 until buffer.height).forEach { row ->
-            val energy = energyBuffer[col, row]
-            val intensity = (255.0 * energy / maxEnergy).toInt()
-            buffer.setRGB(col, row, Color(intensity, intensity, intensity).rgb)
-        }
+    for (pixel in energyBuffer.getSeam()) {
+        buffer.setRGB(pixel.x, pixel.y, Color.RED.rgb)
     }
 
     ImageIO.write(buffer, "png", File(outputImage))
@@ -57,11 +52,13 @@ class BufferedImageEnergyGrid(buffer: BufferedImage) {
     val cols = buffer.width
     val rows = buffer.height
     private val grid = Array(cols) { Array(rows) { 0.0 } }
+    private var seam = Seam(cols, rows)
 
     var maxEnergy: Double = 0.0
     private set
 
     operator fun get(x: Int, y: Int) = grid[x][y]
+    operator fun get(pixel: Pixel) = this[pixel.x, pixel.y]
     operator fun set(x: Int, y: Int, value: Double) {
         grid[x][y] = value
     }
@@ -75,4 +72,42 @@ class BufferedImageEnergyGrid(buffer: BufferedImage) {
             }
         }
     }
+
+    private fun getAdjacent(x: Int, y: Int): List<Pixel> {
+        if (y == 0) return listOf()
+        return (maxOf(x - 1, 0)..minOf(x + 1, cols - 1)).map { Pixel(it, y-1) }
+    }
+
+    fun getSeam(): List<Pixel> {
+
+        for (x in 0 until cols) seam[x, 0] = this[x, 0]
+        for (y in 1 until rows) {
+            for (x in 0 until cols) {
+                seam[x, y] = this[x, y] + getAdjacent(x, y).map { seam[it] }.min()!!
+            }
+        }
+
+        var seamTrackX = seam.grid.mapIndexed { ix, it -> ix to it.last() }.minBy { it.second }!!.first
+        var seamTrack = mutableListOf(Pixel(seamTrackX, rows-1))
+
+        for (y in rows-1 downTo 1) {
+            seamTrackX = getAdjacent(seamTrackX, y).minBy { seam[it] }!!.x
+            seamTrack.add(Pixel(seamTrackX, y-1))
+        }
+
+        return seamTrack
+    }
+
 }
+
+class Seam(val cols: Int, val rows: Int) {
+    var grid = Array(cols) { Array(rows) { Double.POSITIVE_INFINITY }}
+
+    operator fun set(x: Int, y: Int, value: Double) { grid[x][y] = value }
+    operator fun get(x: Int, y: Int) = grid[x][y]
+
+    operator fun set(pixel: Pixel, value: Double) { grid[pixel.x][pixel.y] = value }
+    operator fun get(pixel: Pixel) = grid[pixel.x][pixel.y]
+}
+
+data class Pixel(val x: Int, val y: Int)
